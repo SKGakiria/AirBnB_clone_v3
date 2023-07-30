16f1d8d42f9fa3fefb69e194f30b935a4ca1ec34
Evance Odoyo <odoyoevanc@gmail.com>
2023-07-30 23:50:53 +0300

    remove imported classes which are not used

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -6,8 +6,6 @@ from models import storage
 from models.place import Place
 from models.city import City
 from models.user import User
-from models.amenity import Amenity
-from models.state import State
 
 
 @app_views.route('/cities/<city_id>/places', methods=['GET'],
