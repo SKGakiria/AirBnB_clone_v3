46c8b9a5277ede39481765b12e27c8cf0a148ba3
Evance Odoyo <odoyoevanc@gmail.com>
2023-07-30 21:15:12 +0300

    solve circular dependecy issue on task 3

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
 """The Blueprint for API module"""
 from flask import Blueprint
-from api.v1.views.index import *
-from api.v1.views.amenities import *
-from api.v1.views.users import *
-from views import states
-from views import cities
 
 
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
+
+
+from api.v1.views.index import *
+from api.v1.views.amenities import *
+from api.v1.views.users import *
+from api.v1.views.states import *
+from api.v1.views.cities import *
